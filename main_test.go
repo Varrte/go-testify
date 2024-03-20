4c14979eea587131de4be9f06a77bf2210b92ab8
Vartan Abramov <vartan.spb@gmail.com>
2024-03-20 15:38:08 +0300

    add-four-tests

---
+++ main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("Get", "/cafe?count=5&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	assert.NotEmpty(t, req.Body, "wrong count value")
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+	assert.Len(t, list, totalCount)
+
+}
+
+func TestMainHandlerWrongCity(t *testing.T) {
+
+	req := httptest.NewRequest("Get", "/cafe?count=4&city=wrongcity", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+}
