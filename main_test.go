ed70d787e6fb972480c205c4ca7681c2c3d78a10
Vartan Abramov <vartan.spb@gmail.com>
2024-03-25 14:56:31 +0300

    исправления с учетом замечаний

--- main_test.go
+++ main_test.go
@@ -9,13 +9,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("Get", "/cafe?count=5&city=moscow", nil)
+func recorderAndHandler(req *http.Request) (responseRecorder *httptest.ResponseRecorder) {
 
-	responseRecorder := httptest.NewRecorder()
+	responseRecorder = httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+
+	return responseRecorder
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest(http.MethodGet, "/cafe?count=5&city=moscow", nil)
+
+	responseRecorder := recorderAndHandler(req)
 
 	assert.Equal(t, responseRecorder.Code, http.StatusOK)
 	assert.NotEmpty(t, req.Body, "wrong count value")
@@ -28,11 +35,8 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 
 func TestMainHandlerWrongCity(t *testing.T) {
 
-	req := httptest.NewRequest("Get", "/cafe?count=4&city=wrongcity", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	req := httptest.NewRequest(http.MethodGet, "/cafe?count=4&city=wrongcity", nil)
+	responseRecorder := recorderAndHandler(req)
 
 	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 }
